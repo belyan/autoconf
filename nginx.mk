c0cc94e8ff2197fa604920be449329dfea1090ed
Yuriy Belyakov <belyan@yandex-team.ru>
2011-11-07 17:28:53 +0000

    Обновил Maekfile.
    git-svn-id: svn+ssh://svn.yandex.ru/common-xslt/trunk/autoconf@348 29b10af8-f1e2-11dd-b461-9ef50f9ecc27

--- nginx.mk
+++ nginx.mk
@@ -1,9 +1,30 @@
+NGINX = /etc/init.d/nginx
+CONF_AVAILABLE = /etc/nginx/sites-available
+CONF_ENABLED = /etc/nginx/sites-enabled
+
 all: clean build
 
 clean:
 	-rm -f $(CONF)
 
 build:
-	xsltproc -o ${CONF} nginx.xsl ${META}
+	xsltproc -o ${CONF} autoconf/nginx.xsl ${META}
 
-.PHONY: all clean build
+install:
+	install -m 644 $(CONF) $(CONF_AVAILABLE)
+	ln -sf $(CONF_AVAILABLE)/$(CONF) $(CONF_ENABLED)/$(CONF)
+	make restart
+
+uninstall:
+	-rm -f $(CONF_AVAILABLE)/$(CONF)
+	-rm -f $(CONF_ENABLED)/$(CONF)
+	make restart
+
+restart:
+ifeq ($(shell whoami),root)
+	${NGINX} restart
+else
+	sudo ${NGINX} restart
+endif
+
+.PHONY: all clean build install uninstall restart
